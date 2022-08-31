95fba3d8844b83c742a38d62c3cafea26a6a0dd1
Yaroslav Snigur <yaroslavsnigur@Yaroslavs-MacBook-Air.local>
2022-08-31 17:19:57 -0400

    final commit

--- src/app.ts
+++ src/app.ts
@@ -48,10 +48,22 @@ class ProjectState extends State<Project> {
     addProject(title: string, description: string, numOfPeople: number) {
         const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
         this.projects.push(newProject);
+        this.updateListeners();
+    };
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
         };
-    };
+    }
 }
 
 const projectState = ProjectState.getInstance();
@@ -190,7 +202,11 @@ implements DragTarget {
         } 
     };
 
-    dropHandler(_: DragEvent) {};
+    @autobind
+    dropHandler(event: DragEvent) {
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
+    };
 
     @autobind
     dragLeaveHandler(_: DragEvent) {
